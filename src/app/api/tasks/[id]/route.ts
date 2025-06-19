b2755ada6b4ffe8a835db94b319b9dc42fb397bd
donneyo <donneyo63@gmail.com>
2025-06-19 11:17:29 +0100

    fifth commit after clean up

--- src/app/api/tasks/[id]/route.ts
+++ src/app/api/tasks/[id]/route.ts
@@ -1,20 +1,16 @@
-import type { NextRequest } from 'next/server';
+import { NextRequest } from 'next/server';
 import { connectToDatabase } from '@/lib/mongoose';
 import { Task } from '@/models/todo';
 
-type Context = {
-    params: { id: string }
-};
-
-export async function DELETE(request: NextRequest, context: Context): Promise<Response> {
-    const { id } = context.params;
+export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
+    const { id } = params;
     await connectToDatabase();
     await Task.findByIdAndDelete(id);
     return Response.json({ message: 'Deleted successfully' });
 }
 
-export async function PATCH(request: NextRequest, context: Context): Promise<Response> {
-    const { id } = context.params;
+export async function PATCH(request: NextRequest, { params }: { params: { id: string } }) {
+    const { id } = params;
     const body = await request.json();
     await connectToDatabase();
     const updatedTask = await Task.findByIdAndUpdate(id, body, { new: true });
